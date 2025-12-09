99dc4ad80ee369135a8199ca81985582ce8c0b0d
Muhammadsolih <salikhdev@gmail.com>
2025-12-09 18:31:39 +0500

    Fix bug

--- src/main/java/uz/salikhdev/springbootinfinity/config/Second.java
+++ src/main/java/uz/salikhdev/springbootinfinity/config/Second.java
@@ -2,26 +2,25 @@ package uz.salikhdev.springbootinfinity.config;
 
 public class Second {
 
-  @Bean
-  public String a(){
-    return "hekki";
-  }
+    public String a() {
+        return "hekki";
+    }
 
-  public String login(){
-    return "Login";
-  }
+    public String login() {
+        return "Login";
+    }
 
 
-  public String logout(){
-      return "logout";
-  }
+    public String logout() {
+        return "logout";
+    }
 
-  public String forgot(){
-      return "forgot";
-  }
+    public String forgot() {
+        return "forgot";
+    }
 
-  public String register(){
-    return "register";
-  }
+    public String register() {
+        return "register";
+    }
 
 }
